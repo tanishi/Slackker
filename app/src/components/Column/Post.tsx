91a93c68e2f152868835096f5c8396e1cfcae50f
tanishi <tanishi503@gmail.com>
2017-09-11 23:21:30 +0900

    Modify components to receive message from slack

--- app/src/components/Column/Post.tsx
+++ app/src/components/Column/Post.tsx
@@ -1,11 +1,37 @@
-import * as React from 'React';
+import * as React from 'react';
+import * as Https from 'https';
+import * as WebSocket from 'ws';
+
+import { API_TOKEN } from '../../config';
 
 
 interface Props {
 }
 
+const URL = `https://slack.com/api/rtm.connect?token=${API_TOKEN}`;
+
 export default class Post extends React.Component < any, any > {
+  constructor() {
+    super();
+
+    this.state = { text: 'nico' };
+
+    Https.get(URL, (res) => {
+      res.on('data', (chunk : Buffer) => {
+        const obj = JSON.parse(chunk.toString('utf8'));
+        const ws = new WebSocket(obj.url);
+
+        ws.on('message', (data) => {
+          const json = JSON.parse(data);
+          if (json.type === 'message') {
+            this.setState({ text: json.text });
+          }
+        });
+      });
+    });
+  }
+
   render() {
-    return <div>nico</div>;
+    return <div>{this.state.text}</div>;
   }
 }

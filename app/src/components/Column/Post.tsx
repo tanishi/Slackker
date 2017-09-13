ccebcfa83d716a5f5c0e941effa5a152d7898c45
tanishi <tanishi503@gmail.com>
2017-09-13 23:01:11 +0900

    Change index.tsx with Post component

--- app/src/components/Column/Post.tsx
+++ app/src/components/Column/Post.tsx
@@ -6,32 +6,19 @@ import { API_TOKEN } from '../../config';
 
 
 interface Props {
+  userName: string;
+  text: string;
+  channelName: string;
 }
 
 const URL = `https://slack.com/api/rtm.connect?token=${API_TOKEN}`;
 
-export default class Post extends React.Component < any, any > {
-  constructor() {
-    super();
-
-    this.state = { text: 'nico' };
-
-    Https.get(URL, (res) => {
-      res.on('data', (chunk : Buffer) => {
-        const obj = JSON.parse(chunk.toString('utf8'));
-        const ws = new WebSocket(obj.url);
-
-        ws.on('message', (data) => {
-          const json = JSON.parse(data);
-          if (json.type === 'message') {
-            this.setState({ text: json.text });
-          }
-        });
-      });
-    });
+export default class Post extends React.Component < Props, any > {
+  constructor(props) {
+    this.props = props;
   }
 
   render() {
-    return <div>{this.state.text}</div>;
+    return <div>{this.props.text}</div>;
   }
 }
--- app/src/components/Column/index.tsx
+++ app/src/components/Column/index.tsx
@@ -7,7 +7,27 @@ interface Props {
 }
 
 export default class Column extends React.Component < any, any > {
+  constructor() {
+    super();
+
+    this.state = { messages: [] };
+
+    Https.get(URL, (res) => {
+      res.on('data', (chunk : Buffer) => {
+        const obj = JSON.parse(chunk.toString('utf8'));
+        const ws = new WebSocket(obj.url);
+
+        ws.on('message', (data) => {
+          const json = JSON.parse(data);
+          if (json.type === 'message') {
+            this.setState({ messages: this.state.messages.push(text) });
+          }
+        });
+      });
+    });
+  }
+
   render() {
-    return <Post />;
+    return {this.state.messages.map(message => <Post text={message} />)};
   }
 }

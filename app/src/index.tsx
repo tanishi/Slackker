8035c4f028500fe19efd3b5efb985a70a30c17b4
tanishi <tanishi503@gmail.com>
2017-09-15 20:06:49 +0900

    Tweak line

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,4 +1,5 @@
-import * as React from 'react'; import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import { MuiThemeProvider } from 'material-ui/styles/MuiThemeProvider';
 
 

025c14780ba38e8357c5810bccf4073686a69d8e
tanishi <tanishi503@gmail.com>
2017-09-16 15:49:59 +0900

    Change material-ui version

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,16 +1,33 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { MuiThemeProvider } from 'material-ui/styles/MuiThemeProvider';
-
+import * as Https from 'https';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles/';
 
 import Column from './components/Column';
 
+import { httpsGet } from './utils';
+import Team from './Team';
 import { API_TOKEN, KUMOKUMO_API_TOKEN } from './config';
 
 
+const theme = createMuiTheme();
+
 class App extends React.Component < any, any > {
+  oknctTeam;
   constructor() {
     super();
+
+    this.oknctTeam = new Team(API_TOKEN);
+    httpsGet(`https://slack.com/api/rtm.start?token=${API_TOKEN}`, (data) => {
+      const obj = JSON.parse(data);
+      this.oknctTeam.indexTeamData(obj);
+      this.oknctTeam.resolveName(obj);
+      console.log(this.oknctTeam);
+    });
+
+    httpsGet(`https://slack.com/api/rtm.start?token=${KUMOKUMO_API_TOKEN}`, (data) => {
+      const obj = JSON.parse(data);
+    });
   }
 
   render() {
@@ -18,10 +35,10 @@ class App extends React.Component < any, any > {
     const KUMOKUMO_URL = `https://slack.com/api/rtm.connect?token=${KUMOKUMO_API_TOKEN}`;
 
     return (
-      <MuiThemeProvider>
+      <MuiThemeProvider theme={theme}>
         <div style={{ display: 'flex' }}>
-          <Column url={URL}/>
-          <Column url={KUMOKUMO_URL}/>
+          <Column url={URL} />
+          <Column url={KUMOKUMO_URL} />
         </div>
       </MuiThemeProvider>
     );

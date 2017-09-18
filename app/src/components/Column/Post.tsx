4a7bf3e5de77731fa2c4d9d362a3fa967138cd94
tanishi <tanishi503@gmail.com>
2017-09-18 20:41:54 +0900

    Change Card component to cool ui

--- app/src/components/Column/Post.tsx
+++ app/src/components/Column/Post.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import Card from 'material-ui/Card';
+import { withStyles } from 'material-ui/styles';
 
 
 interface Props {
@@ -7,13 +9,41 @@ interface Props {
   channelName: string;
 }
 
-export default class Post extends React.Component < any, any > {
+const styles = theme => ({
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    marginBottom: 16,
+    fontSize: 14,
+    color: theme.palette.text.secondary,
+  },
+  pos: {
+    marginBottom: 12,
+    color: theme.palette.text.secondary,
+  },
+});
+
+class Post extends React.Component < any, any > {
   constructor(props) {
     super(props);
     this.props = props;
   }
 
   render() {
-    return <div style={{ margin: '20px', border: '5px solid' }}>{this.props.text}</div>;
+    return (
+      <div style={{ margin: '20px' }}>
+        <Card>
+          {this.props.text}
+        </Card>
+      </div>
+    );
   }
 }
+
+export default withStyles(styles)(Post);

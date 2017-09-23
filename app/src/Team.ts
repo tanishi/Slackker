412568b52e43406d2eced8f8419fb4f228db534b
tanishi <tanishi503@gmail.com>
2017-09-24 01:10:57 +0900

    Add class to manage teams

---
+++ app/src/Team.ts
@@ -0,0 +1,22 @@
+export default class Team {
+  token;
+  url;
+  self;
+  index;
+  constructor(token) {
+    this.token = token;
+  }
+
+  indexTeamData(data) {
+    this.url = data.url;
+    this.self = data.self;
+    this.index = {};
+    const keys = ['users', 'channels', 'groups', 'ims', 'bots'];
+    keys.forEach(key => data[key].forEach(obj => this.index[obj.id] = obj));
+    this.index[data.team.id] = data.team;
+  }
+
+  resolveName(id) {
+    return this.index[id].name;
+  }
+}
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -7,38 +7,49 @@ import Column from './components/Column';
 
 import { httpsGet } from './utils';
 import Team from './Team';
-import { API_TOKEN, KUMOKUMO_API_TOKEN } from './config';
+import API_TOKENS from './API_TOKENS';
 
 
 const theme = createMuiTheme();
 
 class App extends React.Component < any, any > {
-  oknctTeam;
+  teams;
+
   constructor() {
     super();
 
-    this.oknctTeam = new Team(API_TOKEN);
-    httpsGet(`https://slack.com/api/rtm.start?token=${API_TOKEN}`, (data) => {
-      const obj = JSON.parse(data);
-      this.oknctTeam.indexTeamData(obj);
-      this.oknctTeam.resolveName(obj);
-      console.log(this.oknctTeam);
-    });
+    this.initialize();
+  }
 
-    httpsGet(`https://slack.com/api/rtm.start?token=${KUMOKUMO_API_TOKEN}`, (data) => {
-      const obj = JSON.parse(data);
+  initialize() {
+    console.log(API_TOKENS);
+    this.teams = [];
+
+    API_TOKENS.map((API_TOKEN, index) => {
+      const START_API = `https://slack.com/api/rtm.start?token=${API_TOKEN}`;
+      this.teams.push(new Team(API_TOKEN));
+
+      httpsGet(START_API, (data) => {
+        const obj = JSON.parse(data);
+        this.teams[index].indexTeamData(obj);
+      });
     });
   }
 
+  columns() {
+    const columns = API_TOKENS.map(API_TOKEN =>
+      <Column key="" url={`https://slack.com/api/rtm.connect?token=${API_TOKEN}`} />);
+    return <div style={{ display: 'flex' }}>{columns}</div>;
+  }
+
   render() {
-    const URL = `https://slack.com/api/rtm.connect?token=${API_TOKEN}`;
-    const KUMOKUMO_URL = `https://slack.com/api/rtm.connect?token=${KUMOKUMO_API_TOKEN}`;
+    const columns = API_TOKENS.map(API_TOKEN =>
+      <Column key="" url={`https://slack.com/api/rtm.connect?token=${API_TOKEN}`} />);
 
     return (
       <MuiThemeProvider theme={theme}>
         <div style={{ display: 'flex' }}>
-          <Column url={URL} />
-          <Column url={KUMOKUMO_URL} />
+          {columns}
         </div>
       </MuiThemeProvider>
     );
